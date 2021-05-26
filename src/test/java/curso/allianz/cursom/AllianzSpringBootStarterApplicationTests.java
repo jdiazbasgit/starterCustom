3541830c720229895c573e01a936326cf7b911ef
Tu Nombre <you@example.com>
2021-05-26 10:58:24 +0200

    carga inicial

---
+++ src/test/java/curso/allianz/cursom/AllianzSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package curso.allianz.cursom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllianzSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
